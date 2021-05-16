f374319b73665ad2ec5dafceceb20cf03d3bbc97
TYSNinja <80488241+TYSNinja@users.noreply.github.com>
2021-05-16 17:49:01 -0400

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -6,13 +6,13 @@ target_port= "SERVER PORT"  #set this variable
 my_client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 #connect to srv
-client.connect((target_srv,target_srv))
+my_client.connect((target_srv,target_srv))
 
 #send some test data
-client.send("GET / HTTP/1.1\r\nHOST: mytestserver.com\r\n\r\n")
+my_client.send("GET / HTTP/1.1\r\nHOST: mytestserver.com\r\n\r\n")
 
 #get response
-response = client.receive(4096)
+response = my_client.recv(4096)
 
 #print response 
 print(response)
